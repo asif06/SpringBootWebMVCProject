cae1bf52ae25e0b3e3e7da57c38ee33e6d58c17d
Asif <Asif@LAPTOP-B8FFI9AP>
2018-12-10 17:14:54 +0530

    First Commit

---
+++ SpringBootWebMvcProject/src/main/java/com/example/webmvc/SpringBootWebMvcProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebMvcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebMvcProjectApplication.class, args);
+	}
+}
